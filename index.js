ed545804ea19609e3e2d14afd17b7b79dc5052de
karljorgent <karl-jorgen.tormet@khk.ee>
2022-10-10 23:28:13 +0300

    Created form request responce with API data

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
 const path = require('path')
 const fetch = require('node-fetch')
+const bp = require('body-parser')
 
+// view engine
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+// body-parser
+app.use(bp.json())
+app.use(bp.urlencoded({ extended: true }))
+
+// weather key and city
 const key = '09c55a9a2af84d1398ebb65835dd5b72'
 let city = 'Tartu'
 
+// get weather data for Tartu
 app.get('/', function (req, res) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
     .then((responce) => {
@@ -20,9 +27,25 @@ app.get('/', function (req, res) {
         let description = data.weather[0].description
         let city = data.name
         let temp = Math.round(parseFloat(data.main.temp) - 273.15)
-        console.log(description);
-        console.log(city);
-        console.log(temp);
+        res.render('index', {
+            description: description,
+            city: city,
+            temp: temp
+        })
+    })
+})
+
+// Change data to your liking
+app.post('/', function(req, res){
+    let city = req.body.cityname
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+    .then((responce) => {
+        return responce.json()
+    })
+    .then((data) => {
+        let description = data.weather[0].description
+        let city = data.name
+        let temp = Math.round(parseFloat(data.main.temp) - 273.15)
         res.render('index', {
             description: description,
             city: city,
@@ -32,5 +55,5 @@ app.get('/', function (req, res) {
 })
 
 app.listen(port, () => {
-    console.log(`Now listening on port ${port}`)
+    console.log(`Now listening on port http://localhost:${port}`)
 });
